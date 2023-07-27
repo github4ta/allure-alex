c15d2dced59f7beee1191f172c90517b78f25d79
Sergei Tsarik <serge.tsarik@gmail.com>
2023-07-27 21:02:55 +0300

    refactor tests

--- src/test/java/com/tvmaze/ui/test/TVShowCalendarTest.java
+++ src/test/java/com/tvmaze/ui/test/TVShowCalendarTest.java
@@ -3,10 +3,12 @@ package com.tvmaze.ui.test;
 import com.tvmaze.ui.data.Data;
 import com.tvmaze.ui.steps.TVShowCalendarStep;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class TVShowCalendarTest extends AbstractTest {
 
+    @Ignore
     @Test(description = "UI ShowCalendar test",
             dataProvider = "calendarDateAndTVShowName", dataProviderClass = Data.class)
     public void testCalendarContainsShowOnDate(String date, String tvShowName) {
